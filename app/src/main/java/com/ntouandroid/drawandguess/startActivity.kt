a9ca441a8b9e924fa3836b1c806821c1d03d1264
KevinHo <kevin06026@gmail.com>
2020-04-25 17:11:22 +0800

    start createRoom joinRoom activity

---
+++ app/src/main/java/com/ntouandroid/drawandguess/startActivity.kt
@@ -0,0 +1,12 @@
+package com.ntouandroid.drawandguess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class startActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
