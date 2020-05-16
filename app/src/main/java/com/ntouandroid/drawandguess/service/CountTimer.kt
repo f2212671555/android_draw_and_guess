a8a70a087db11aaddf90d55883412b82941e7f12
KevinHo <kevin06026@gmail.com>
2020-05-16 20:05:02 +0800

    countTimer

---
+++ app/src/main/java/com/ntouandroid/drawandguess/service/CountTimer.kt
@@ -0,0 +1,22 @@
+package com.ntouandroid.drawandguess.service
+
+import android.os.CountDownTimer
+
+class CountTimer(second: Long, countDown: Long) : CountDownTimer(second * 1000, countDown * 1000) {
+    override fun onTick(millisUntilFinished: Long) {
+        //test_tv.text = "seconds remaining: " + millisUntilFinished / 1000
+    }
+
+    override fun onFinish() {
+        //test_tv.text = "done!"
+    }
+
+
+    fun startCount() {
+        this.start()
+    }
+
+    fun cancelCount() {
+        this.cancel()
+    }
+}
