eb2a54260fec0d4f3f746fd635a2e2568ae27314
jaja <arthur870704@gmail.com>
2020-05-23 22:18:45 +0800

    all test ui (.kt/.xml) update

---
+++ app/src/main/java/com/ntouandroid/drawandguess/webSocket/PaintActivity.kt
@@ -0,0 +1,13 @@
+package com.ntouandroid.drawandguess.webSocket
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ntouandroid.drawandguess.R
+
+class PaintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_paint)
+    }
+
+}
