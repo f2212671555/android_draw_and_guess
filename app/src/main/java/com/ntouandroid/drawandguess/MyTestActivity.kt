fe113f67f04b39d4b6896f699e483039df9aeae3
陸顥壬 <f2212671555@gmail.com>
2020-05-24 01:27:08 +0800

    改

--- app/src/main/java/com/ntouandroid/drawandguess/MyTestActivity.kt
+++ app/src/main/java/com/ntouandroid/drawandguess/MyTestActivity.kt
@@ -3,20 +3,32 @@ package com.ntouandroid.drawandguess
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import com.ntouandroid.drawandguess.colorPicker.PaintActivity
 
 class MyTestActivity: AppCompatActivity() {
 
     lateinit var tv_test:TextView
-
+    lateinit var btnTestPaint:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_test)
 
         tv_test = findViewById(R.id.tv_test)
 
+        btnTestPaint = findViewById(R.id.btn_testPaint)
 
-
+        btnTestPaint.setOnClickListener { goTestPaintBtn() }
     }
+
+    private fun goTestPaintBtn() {
+        val intent = Intent(this,PaintActivity::class.java)
+        intent.putExtra("roomid","123room")
+        intent.putExtra("userid","123user")
+        startActivity(intent)
+    }
+
 
 }
--- app/src/main/java/com/ntouandroid/drawandguess/colorPicker/PaintActivity.kt
+++ app/src/main/java/com/ntouandroid/drawandguess/colorPicker/PaintActivity.kt
@@ -1,12 +1,15 @@
 package com.ntouandroid.drawandguess.colorPicker
 
+import android.app.Dialog
 import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.SeekBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.drawtest.ColorPaint
 import com.ntouandroid.drawandguess.R
@@ -20,10 +23,12 @@ class PaintActivity : AppCompatActivity() {
     lateinit var size: Button
     var sizeNumGet: Float = 30.0f
     lateinit var clean: Button
-
-
-
-
+    lateinit var sizeNumPrint: EditText
+    lateinit var etMessage: EditText
+    lateinit var tvMessage: TextView
+    lateinit var sizeNum: SeekBar
+    lateinit var sizeCancelBtn: Button
+    lateinit var sizeOkBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,26 +36,40 @@ class PaintActivity : AppCompatActivity() {
 
         eraser = findViewById(R.id.eraser)
         size = findViewById(R.id.size)
+        etMessage = findViewById(R.id.message_et)
+        tvMessage = findViewById(R.id.tv_recieve)
+        clean = findViewById(R.id.backgroundClean)
+        val openDialog = Dialog(this)
+        openDialog.setContentView(R.layout.sizechange)
+        sizeNumPrint = openDialog.findViewById(R.id.sizeNumPrint)
+        sizeCancelBtn = openDialog.findViewById(R.id.sizeCancelBtn)
+        sizeOkBtn = openDialog.findViewById(R.id.sizeOkBtn)
 
         btnColorSelected.setOnClickListener {
             colorSelector.visibility = View.VISIBLE
         }
 
-        eraser.setOnClickListener{ eraserFun() }
-        size.setOnClickListener{ sizeChange() }
-        clean.setOnClickListener{ backgroundClean() }
+        sizeNum = openDialog.findViewById(R.id.sizeNum)
+        eraser.setOnClickListener { eraserFun() }
+        size.setOnClickListener { sizeChange() }
+        clean.setOnClickListener { backgroundClean() }
+
 
         sizeNumPrint.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable) {
                 sizeNum.progress = Integer.parseInt(s.toString())
             }
 
-            override fun beforeTextChanged(s: CharSequence, start: Int,
-                                           count: Int, after: Int) {
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
             }
 
-            override fun onTextChanged(s: CharSequence, start: Int,
-                                       before: Int, count: Int) {
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
 
             }
         })
@@ -58,23 +77,27 @@ class PaintActivity : AppCompatActivity() {
         strColor.addTextChangedListener(object : TextWatcher {
 
             override fun afterTextChanged(s: Editable) {
-                if (s.length == 6){
+                if (s.length == 6) {
                     colorR.progress = Integer.parseInt(s.substring(0..1), 16)
                     colorG.progress = Integer.parseInt(s.substring(2..3), 16)
                     colorB.progress = Integer.parseInt(s.substring(4..5), 16)
-                } else if (s.length == 8){
+                } else if (s.length == 8) {
                     colorR.progress = Integer.parseInt(s.substring(2..3), 16)
                     colorG.progress = Integer.parseInt(s.substring(4..5), 16)
                     colorB.progress = Integer.parseInt(s.substring(6..7), 16)
                 }
             }
 
-            override fun beforeTextChanged(s: CharSequence, start: Int,
-                                           count: Int, after: Int) {
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
             }
 
-            override fun onTextChanged(s: CharSequence, start: Int,
-                                       before: Int, count: Int) {
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
 
             }
         })
@@ -83,10 +106,12 @@ class PaintActivity : AppCompatActivity() {
         colorR.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onStopTrackingTouch(seekBar: SeekBar) {}
             override fun onStartTrackingTouch(seekBar: SeekBar) {}
-            override fun onProgressChanged(seekBar: SeekBar, progress: Int,
-                                           fromUser: Boolean) {
+            override fun onProgressChanged(
+                seekBar: SeekBar, progress: Int,
+                fromUser: Boolean
+            ) {
                 val colorStr = getColorString()
-                strColor.setText(colorStr.replace("#","").toUpperCase())
+                strColor.setText(colorStr.replace("#", "").toUpperCase())
                 btnColorPreview.setBackgroundColor(Color.parseColor(colorStr))
             }
         })
@@ -95,10 +120,12 @@ class PaintActivity : AppCompatActivity() {
         colorG.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onStopTrackingTouch(seekBar: SeekBar) {}
             override fun onStartTrackingTouch(seekBar: SeekBar) {}
-            override fun onProgressChanged(seekBar: SeekBar, progress: Int,
-                                           fromUser: Boolean) {
+            override fun onProgressChanged(
+                seekBar: SeekBar, progress: Int,
+                fromUser: Boolean
+            ) {
                 val colorStr = getColorString()
-                strColor.setText(colorStr.replace("#","").toUpperCase())
+                strColor.setText(colorStr.replace("#", "").toUpperCase())
                 btnColorPreview.setBackgroundColor(Color.parseColor(colorStr))
             }
         })
@@ -107,10 +134,12 @@ class PaintActivity : AppCompatActivity() {
         colorB.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onStopTrackingTouch(seekBar: SeekBar) {}
             override fun onStartTrackingTouch(seekBar: SeekBar) {}
-            override fun onProgressChanged(seekBar: SeekBar, progress: Int,
-                                           fromUser: Boolean) {
+            override fun onProgressChanged(
+                seekBar: SeekBar, progress: Int,
+                fromUser: Boolean
+            ) {
                 val colorStr = getColorString()
-                strColor.setText(colorStr.replace("#","").toUpperCase())
+                strColor.setText(colorStr.replace("#", "").toUpperCase())
                 btnColorPreview.setBackgroundColor(Color.parseColor(colorStr))
             }
         })
@@ -126,10 +155,12 @@ class PaintActivity : AppCompatActivity() {
         sizeNum.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onStopTrackingTouch(seekBar: SeekBar) {}
             override fun onStartTrackingTouch(seekBar: SeekBar) {}
-            override fun onProgressChanged(seekBar: SeekBar, progress: Int,
-                                           fromUser: Boolean) {
+            override fun onProgressChanged(
+                seekBar: SeekBar, progress: Int,
+                fromUser: Boolean
+            ) {
                 val sizeGet = Integer.toHexString(sizeNum.progress)
-                sizeNumPrint.setText(sizeGet.replace("#","").toUpperCase())
+                sizeNumPrint.setText(sizeGet.replace("#", "").toUpperCase())
             }
         })
 
@@ -138,7 +169,7 @@ class PaintActivity : AppCompatActivity() {
         }
 
         sizeOkBtn.setOnClickListener {
-            val color:String = getColorString()
+            val color: String = getColorString()
             setColor()
             btnColorSelected.setBackgroundColor(Color.parseColor(color))
             colorSelector.visibility = View.GONE
@@ -148,39 +179,48 @@ class PaintActivity : AppCompatActivity() {
         userid = intent.getStringExtra("userid")
 
 
-
-
     }
+
     companion object {
-        var colorpaint = ColorPaint(0,0,0, 30.0f)
+        var colorpaint = ColorPaint(0, 0, 0, 30.0f)
         var roomid: String = ""
         var userid: String = ""
         var nextid: String = ""
     }
 
-    fun eraserFun(){
-        colorpaint = ColorPaint(0,0,0, 30.0f)
+    fun eraserFun() {
+        colorpaint = ColorPaint(0, 0, 0, 30.0f)
     }
 
-    fun sizeChange(){
-        colorpaint = ColorPaint(((255 * colorR.progress) / colorR.max),((255 * colorG.progress) / colorG.max),((255 * colorB.progress) / colorB.max), sizeNumGet)
+    fun sizeChange() {
+        colorpaint = ColorPaint(
+            ((255 * colorR.progress) / colorR.max),
+            ((255 * colorG.progress) / colorG.max),
+            ((255 * colorB.progress) / colorB.max),
+            sizeNumGet
+        )
     }
 
-    fun setColor(){
-        colorpaint = ColorPaint(((255 * colorR.progress) / colorR.max),((255 * colorG.progress) / colorG.max),((255 * colorB.progress) / colorB.max), sizeNumGet)
+    fun setColor() {
+        colorpaint = ColorPaint(
+            ((255 * colorR.progress) / colorR.max),
+            ((255 * colorG.progress) / colorG.max),
+            ((255 * colorB.progress) / colorB.max),
+            sizeNumGet
+        )
     }
 
-    fun backgroundClean(){
+    fun backgroundClean() {
 
     }
 
     fun getColorString(): String {
-        var r = Integer.toHexString(((255*colorR.progress)/colorR.max))
-        if(r.length==1) r = "0"+r
-        var g = Integer.toHexString(((255*colorG.progress)/colorG.max))
-        if(g.length==1) g = "0"+g
-        var b = Integer.toHexString(((255*colorB.progress)/colorB.max))
-        if(b.length==1) b = "0"+b
+        var r = Integer.toHexString(((255 * colorR.progress) / colorR.max))
+        if (r.length == 1) r = "0" + r
+        var g = Integer.toHexString(((255 * colorG.progress) / colorG.max))
+        if (g.length == 1) g = "0" + g
+        var b = Integer.toHexString(((255 * colorB.progress) / colorB.max))
+        if (b.length == 1) b = "0" + b
         return "#" + r + g + b
     }
 }
